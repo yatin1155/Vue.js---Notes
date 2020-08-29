c388ed996d93e1e2106e383fe24aeba9cc5c22b0
Yatin <Yatin.yatin.con@derivco.com>
2020-08-29 19:52:57 +0530

    Communication with callback fn

--- Sections/components/Communication/src/components/User.vue
+++ Sections/components/Communication/src/components/User.vue
@@ -6,7 +6,10 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :name='name'></app-user-detail>
+                <app-user-detail 
+                :name='name' 
+                @nameWasReset='name = $event'
+                :resetFn='resetName'></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -28,6 +31,9 @@
         methods:{
             changeName(){
                this.name = 'Mr.Kapoor' 
+            },
+            resetName(){
+                this.name = 'Yatin'
             }
         },
         components: {
--- Sections/components/Communication/src/components/UserDetail.vue
+++ Sections/components/Communication/src/components/UserDetail.vue
@@ -4,17 +4,24 @@
         <p>Many Details</p>
         <p>User Name: {{name}}</p>
         <p>Reversed Name: {{switchName()}}</p>
+        <button @click="resetName">RESET Name</button>
+        <button @click="resetFn()">RESET Name</button>
     </div>
 </template>
 
 <script>
     export default {
         props:{
-            name: String
+            name: String,
+            resetFn: Function
         },
         methods:{
             switchName(){
                 return this.name.split('').reverse().join("");
+            },
+            resetName(){
+                this.name = 'Yatin';
+                this.$emit('nameWasReset',this.name)
             }
         }
     }
