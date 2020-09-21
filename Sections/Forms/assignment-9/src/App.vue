b1b006b61a206af25300a38883c31918bec40ef5
Yatin <Yatin.yatin.con@derivco.com>
2020-09-21 20:59:01 +0530

    Changes before making custom control

--- Sections/Forms/assignment-9/src/App.vue
+++ Sections/Forms/assignment-9/src/App.vue
@@ -1,47 +1,100 @@
 <template>
-    <div class="container">
+  <div class="container">
+    <div v-if="!isSumbitted">
         <form>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
+                <!-- Exercise 1 -->
+                <!-- Create a Signup Form where you retrieve the following Information -->
+                <!-- Full Name (First Name + Last Name) -->
+                <!-- Mail -->
+                <!-- Password -->
+                <!-- Store Data? Yes/No -->
 
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+                <h1>User Info Form</h1>
+                <hr />
+                <div class="form-group">
+                    <label for="firstName">First Name</label>
+                    <input type="text" id="firstName" class="form-control" v-model.lazy="userData.firstName" />
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" id="lastName" class="form-control" v-model.lazy="userData.lastName" />
+                </div>
+                <div class="form-group">
+                    <label for="email">Mail</label>
+                    <input type="email" id="email" class="form-control" v-model.lazy="userData.email" />
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" class="form-control" v-model.lazy="userData.password" />
+                </div>
+                <div>
+                    <label for="Yes">Store Data :</label>
+                    <label for="yes">
+                    <input type="radio" id="yes" value="Yes" v-model.lazy="userData.storeData" /> Yes
+                    </label>
+                    <label for="no">
+                    <input type="radio" id="no" value="No" v-model.lazy="userData.storeData" /> No
+                    </label>
+                </div>
 
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
+                <!-- Exercise 2 -->
+                <!-- Only display the Form if it has NOT been submitted -->
+                <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                <!-- Exercise 3 -->
+                <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <button class="btn btn-primary"
+                @click.prevent="onSubmit">Submit!</button>
                 </div>
             </div>
         </form>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
+    <hr />
+    <div class="row" v-if="isSumbitted">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: {{userData.firstName}} {{userData.lastName}}</p>
+            <p>Mail: {{userData.email}}</p>
+            <p>Password: {{userData.password}}</p>
+            <p>Store in Database?: {{userData.storeData}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {
+  data() {
+    return {
+      userData: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        storeData: "Yes"
+      },
+      isSumbitted: false
+    };
+  },
+  methods :{
+      onSubmit(){
+          this.isSumbitted = true;
+      }
+  }
+};
 </script>
 
 <style>
