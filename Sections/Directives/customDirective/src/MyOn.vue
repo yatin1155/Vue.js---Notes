a350a6c2d8071e7f07a4c2eeb8460b5b39173495
Yatin <Yatin.yatin.con@derivco.com>
2020-09-22 02:08:44 +0530

    Assignemnt for custom directives

---
+++ Sections/Directives/customDirective/src/MyOn.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p v-customOn:click="alertMe">Click me</p>
+    </div>
+</template>
+
+<script>
+export default {
+    directives:{
+        "customOn":{
+            bind(el, binding){
+                const event = binding.arg;
+                const fn = binding.value;
+                el.addEventListener(event, fn);
+                
+            }
+        }
+    },
+    methods:{
+        alertMe(){
+            alert("Hello");
+        }
+    }
+}
+</script>
--- Sections/Directives/customDirective/src/main.js
+++ Sections/Directives/customDirective/src/main.js
@@ -1,7 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import MyOn from './MyOn.vue'
+
+Vue.directive('highlight',{
+  bind(el, binding, vnode){
+    // el.style.backgroundColor = 'pink';
+    // el.style.backgroundColor = binding.value;
+
+    var delay = 0;
+    if(binding.modifiers['delay']){
+      delay = 5000;
+    }
+    setTimeout(()=>{
+      if(binding.arg == "background"){
+        el.style.backgroundColor = binding.value;
+      }else{
+        el.style.color = binding.value;
+      }
+    },delay)
+   
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+new Vue({
+  el: '#myon',
+  render: h => h(MyOn)
 })
